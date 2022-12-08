85089fc3b2a7eecdb9591b79e60921332214f5e1
Luis Felipe Maciel <luis.barbosa@al.infnet.edu.br>
2022-12-08 20:47:15 -0300

    Definindo novas constantes de classificação do gamer"

--- src/main/java/br/edu/infnet/esports/model/auxiliar/Constante.java
+++ src/main/java/br/edu/infnet/esports/model/auxiliar/Constante.java
@@ -5,5 +5,10 @@ public class Constante {
 	public final static int PESO_1 = 1;
 	public final static int PESO_2 = 2;
 	public final static int PESO_3 = 3;
+	
+	public final static String INICIANTE = "iniciante";
+	public final static String SEMIPRO = "semipro";
+	public final static String PROFISSIONAL = "profissional";
+	public final static String LENDARIO = "lendário";
 
 }
