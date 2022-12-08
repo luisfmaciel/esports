a854565ac07378bd682bc2cc2b8a7018593e4d4e
Luis Felipe Maciel <luis.barbosa@al.infnet.edu.br>
2022-12-08 17:22:25 -0300

    Criação da classe 'Constante'

---
+++ src/main/java/br/edu/infnet/esports/model/auxiliar/Constante.java
@@ -0,0 +1,9 @@
+package br.edu.infnet.esports.model.auxiliar;
+
+public class Constante {
+	
+	public final static int PESO_1 = 1;
+	public final static int PESO_2 = 2;
+	public final static int PESO_3 = 3;
+
+}
