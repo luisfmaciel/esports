3ac36078bd838b89d58ac9fa7651443bfec74094
Luis Felipe Maciel <luis.barbosa@al.infnet.edu.br>
2022-12-08 18:18:14 -0300

    Implementação de teste

--- src/main/java/br/edu/infnet/esports/model/tests/GamerTeste.java
+++ src/main/java/br/edu/infnet/esports/model/tests/GamerTeste.java
@@ -6,23 +6,26 @@ public class GamerTeste {
 
 	public static void main(String[] args) {
 		
-		Gamer gamer1 = new Gamer();
-		Gamer gamer2 = new Gamer();
-		Gamer gamer3 = new Gamer();
-		
-		Fifa fifa = new Fifa();
-	
-		Equipe equipe1 = new Equipe(); 
 		
 		try {
+			Gamer gamer1 = new Gamer();
+			Gamer gamer2 = new Gamer();
+			Gamer gamer3 = new Gamer();
+			
+			Fifa fifa = new Fifa("PS5", 70, 70, 70);
+			
+			Equipe equipe1 = new Equipe(); 
+
 			gamer1.setEmail("luis@esports.com");
 			gamer2.setEmail("felipe@esports.com");
 			gamer3.setEmail("maciel@esports.com");
 
 			fifa.setPlataforma("PS5");
-			fifa.setNivel("amador");
+			fifa.setNivel(fifa.calculaNivelGamer());
+			
 			
 			System.out.println(fifa.toString());
+			System.out.println(fifa.getNivel());
 
 			gamer1.setGames(fifa);
 			gamer2.setGames(fifa);
@@ -32,7 +35,7 @@ public class GamerTeste {
 			equipe1.setLimiteParticipantes(3);
 			equipe1.setGame(fifa);
 			equipe1.setPlataforma("PS5");
-			equipe1.setNivel("amador");
+			equipe1.setNivel("profissional");
 				
 			equipe1.setGamers(gamer1);
 			equipe1.setGamers(gamer2);
