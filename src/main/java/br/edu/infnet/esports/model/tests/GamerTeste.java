c742e6559836e7019ae16ab5cae3257a550e4828
Luis Felipe Maciel <luis.barbosa@al.infnet.edu.br>
2022-12-10 18:54:49 -0300

    Criação da classe de testes para o Gamer

--- src/main/java/br/edu/infnet/esports/model/tests/GamerTeste.java
+++ src/main/java/br/edu/infnet/esports/model/tests/GamerTeste.java
@@ -1,59 +1,56 @@
 package br.edu.infnet.esports.model.tests;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.edu.infnet.esports.model.domain.*;
 
 public class GamerTeste {
 
 	public static void main(String[] args) {
 		
-		
 		try {
-			Gamer gamer1 = new Gamer();
-			Gamer gamer2 = new Gamer();
-			Gamer gamer3 = new Gamer();
+			List<Game> gamesLuis = new ArrayList<Game>();
 			
-			Fifa fifa = new Fifa("PS5", 70, 70, 70);
+			gamesLuis.add(new Fifa("PS5"));
 			
-			Equipe equipe1 = new Equipe(); 
-
-			gamer1.setEmail("luis@esports.com");
-			gamer2.setEmail("felipe@esports.com");
-			gamer3.setEmail("maciel@esports.com");
-
-			fifa.setPlataforma("PS5");
-			fifa.setNivel(fifa.calculaNivelGamer());
+			Gamer gamerLuis = new Gamer("Luis", "luisinho@esports.com", 9, gamesLuis);
 			
-			
-			System.out.println(fifa.toString());
-			System.out.println(fifa.getNivel());
-
-			gamer1.setGames(fifa);
-			gamer2.setGames(fifa);
-			gamer3.setGames(fifa);
-			
-			equipe1.setNome("Futiba");
-			equipe1.setLimiteParticipantes(3);
-			equipe1.setGame(fifa);
-			equipe1.setPlataforma("PS5");
-			equipe1.setNivel("profissional");
-				
-			equipe1.setGamers(gamer1);
-			equipe1.setGamers(gamer2);
-			equipe1.setGamers(gamer3);
-			
-			Gamer.setGamersTotal(gamer1);
-			Gamer.setGamersTotal(gamer2);
-			Gamer.setGamersTotal(gamer3);
-		
-//			for(Gamer gamer : Gamer.getGamersTotal()) {
-//				for(Game game : gamer.getGames()) System.out.println(game.getNome());
-//			}
-			
-			System.out.println(fifa.getNome());
+			System.out.println(gamerLuis);
 			
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+		
+		try {
+			List<Game> gamesFelipe = new ArrayList<Game>();
+			
+			gamesFelipe.add(new Fifa("PS5"));
+			gamesFelipe.add(new CsGo("PC"));
+			
+			Gamer gamerFelipe = new Gamer("Felipe", "felipinho@esports.com", 9, gamesFelipe);
+			
+			System.out.println(gamerFelipe);
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		try {
+			List<Game> gamesBarbosa = new ArrayList<Game>();
+			
+			gamesBarbosa.add(new Dungeons("PS5"));
+			gamesBarbosa.add(new Fifa("PS5"));
+			gamesBarbosa.add(new CsGo("PC"));
+			
+			Gamer gamerBarbosa= new Gamer("Barbosa", "barbosinha@esports.com", 9, gamesBarbosa);
+			
+			System.out.println(gamerBarbosa);
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
 		
 	}
 
